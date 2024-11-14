6326a11b5224b495d55d7cc2457ec8aea61e442f
alja1313 <olgaslahota@gmail.com>
2024-11-14 15:47:09 +0100

    Created a Search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import logo2 from "./logo2.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo2} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo2} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This applications was built by <strong>Olga Slahota</strong> ⭐
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
