9004a71d1437d1171c05d557c32247e8d30abfeb
alja1313 <olgaslahota@gmail.com>
2024-11-14 14:47:29 +0100

    Change dictionary logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import logo from "./logo.png";
+import logo2 from "./logo2.png";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <img src={logo2} className="App-logo img-fluid" alt="logo" />
       </header>
     </div>
   );
